59e3adf55ea668ac167398144135625e3d41a01a
Martin Vystcil <vystrcil.martin@gmail.com>
2017-02-23 21:18:23 +0100

    Alter main method

--- src/main.cpp
+++ src/main.cpp
@@ -14,20 +14,8 @@ std::string lf = "\n";
 
 int main(int argc, char* argv[])
 {
-    /*ThermometerStatistics stats;
-    Thermometer th1("AA");
-    Thermometer th2("BB");
-
-    dbg << "Starting program to collect all incoming data" << lf;
-    ThermometersList& list = ThermometersList::getInstance();
-
-    list.registerThermometer(th1);
-    list.registerThermometer(th2);
-
-    stats.startStatsColl();*/
     Supervision supervision;
     
-    //dbg << "Create supervision thread" << lf;
     std::thread main(&Supervision::init, &supervision);
     main.join();
     dbg << "Exiting" << lf;
