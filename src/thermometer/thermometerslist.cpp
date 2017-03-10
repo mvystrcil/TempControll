4d7f3c5eb0c81458f1f9604b1d4324d02a58704b
martin <vystrcil.martin@gmail.com>
2017-03-10 13:33:15 +0100

    Reformat code

--- src/thermometer/thermometerslist.cpp
+++ src/thermometer/thermometerslist.cpp
@@ -13,52 +13,52 @@ ThermometersList::~ThermometersList()
 
 ThermometersList& ThermometersList::getInstance()
 {
-	static ThermometersList instance;
-    return instance;
+  static ThermometersList instance;
+  return instance;
 }
 
 void ThermometersList::resetInstance()
 {
-	// Do nothing right now
+  // Do nothing right now
 }
 
 bool ThermometersList::registerThermometer(Thermometer *thermometer)
 {
-    vector<Thermometer *>::iterator iterator;
-
-    for(iterator = this->meters.begin(); iterator != this->meters.end(); iterator++)
+  vector<Thermometer *>::iterator iterator;
+  
+  for(iterator = this->meters.begin(); iterator != this->meters.end(); iterator++)
+  {
+    Thermometer *th = *iterator;
+    if(th == thermometer)
     {
-        Thermometer *th = *iterator;
-        if(th == thermometer)
-        {
-            return false;
-        }
+      return false; 
     }
+  }
 
-    dbg << "Append thermometer " << thermometer->getThermometerName();
-    this->meters.push_back(thermometer);
-
-    return true;
+  dbg << "Append thermometer " << thermometer->getThermometerName();
+  this->meters.push_back(thermometer);
+  
+  return true;
 }
 
 bool ThermometersList::unregisterThermometer(const Thermometer* thermometer)
 {
-    vector<Thermometer *>::iterator it;
-
-    for(it = this->meters.begin(); it != this->meters.end(); it++)
+  vector<Thermometer *>::iterator it;
+  
+  for(it = this->meters.begin(); it != this->meters.end(); it++)
+  {
+    Thermometer *th = *it;
+    if(th == thermometer)
     {
-        Thermometer *th = *it;
-        if(th == thermometer)
-        {
-            this->meters.erase(it);
-            return true;
-        }
+      this->meters.erase(it);
+      return true;
     }
-
-    return false;
+  }
+  
+  return false;
 }
 
 vector<Thermometer *> ThermometersList::getRegisteredList() const
 {
-    return this->meters;
+  return this->meters;
 }
