706dd27a0d08b1a82e505df37aa47e6976915f39
Martin Vystcil <vystrcil.martin@gmail.com>
2017-03-23 17:18:12 +0100

    Add argument for unit tests to enable console output

--- src/units/testscript.cpp
+++ src/units/testscript.cpp
@@ -1,14 +1,41 @@
 #include <cppunit/TestSuite.h>
 #include <cppunit/ui/text/TestRunner.h>
- 
+#include <unistd.h>
+
 #include "thermometertest.h"
 #include "timerlibtest.h"
 #include "databasetest.h"
 #include "../lib/logger.h"
- 
-int main() {
+
+static bool consoleEnabled = false;
+
+bool parseInputArgs(int argc, char *argv[])
+{
+  char opt;
+  std::string optArgument = "";
+  
+  dbg << "Parsing input arguments";
+  
+  while((opt = getopt(argc, argv, "c:")) != -1)
+  {
+    switch(opt)
+    {
+      case 'c':
+	consoleEnabled = true;
+	break;
+      
+      default:
+	warn << "Unknown program argument " << opt;return false;
+	break;
+    }
+  }
+  
+  return true;
+}
+
+int main(int argc, char *argv[]) { 
   Logger::setReportingLevel(DEBUG);
-  Logger::setLogToConsole(false);
+  Logger::setLogToConsole(consoleEnabled);
   Logger::setLogToFile("units.log");
   
   CppUnit::TextUi::TestRunner runner;
